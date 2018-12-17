d72f66ab29ba8a55f59be9e21dbd37ce59a1b75a
CephHunter <32884861+CephHunter@users.noreply.github.com>
2018-12-17 21:56:43 +0100

    +

--- main.m
+++ main.m
@@ -1,10 +1,9 @@
 %% AM modulatie
 Fs = 10000;
-t_length = 3; % seconds
+t_length = 6; % seconds
 t = 0:1/Fs:t_length-1/Fs;
 N = length(t);
-signal = sin(2*pi*t).*cos(2*pi*3*t);
-% carrier = sin(2*pi*100*t);
+signal = 0.5*sin(2*pi*t)+0.3*cos(2*pi*5*t);
 f_carrier = 100;
 subplot(3,2,1)
 plot(t,signal)
@@ -12,7 +11,7 @@ title("Signal")
 
 RF_0 = cos(2*pi*f_carrier*t);
 RF_90 = sin(2*pi*f_carrier*t);
-I_in = (0) .* RF_0;
+I_in = 0 .* RF_0;
 Q_in = (1+signal) .* RF_90;
 RF_signal = I_in + Q_in;
 
@@ -28,13 +27,19 @@ fshift = (-N/2:N/2-1)*Fs/N;
 yshift = fftshift(y);
 subplot(3,2,3)
 plot(fshift, abs(yshift))
-axis([0 150 0 15000])
+axis([75 125 0 t_length*Fs/2])
 title("fft")
 
 % Am demodulation
 I = cos(2*f_carrier*pi*t).*modulated_signal;
 Q = sin(2*f_carrier*pi*t).*modulated_signal;
 received_signal = movmax(sqrt(I.^2 + Q.^2), Fs/2/f_carrier);
+
+a=1;
+b=repelem(1/(Fs/2/f_carrier), Fs/2/f_carrier);
+I=filter(b,a,I);
+Q=filter(b,a,Q);
+received_signal=filter(b,a,received_signal);
 
 subplot(3,2,4)
 plot(t,I)
@@ -47,10 +52,43 @@ plot(t,received_signal)
 title("Recieved signal")
 
 %% FM modulatie
+clear;
+clc;
+Fs = 1000;
+t_length = 6; % seconds
+t = 0:1/Fs:t_length-1/Fs;
+N = length(t);
+signal = sin(2*pi*t)+0.5*cos(2*pi*5*t);
+f_carrier = 100;
+subplot(5,2,1:2)
+plot(t,signal)
+title('signal')
 
+RF_signal = cos(2*pi*f_carrier*t + 1*signal);
+subplot(5,2,3:4)
+plot(t, RF_signal)
+title('RF modulated')
 
-
-
+I = cos(2*f_carrier*pi*t).*RF_signal;
+Q = sin(2*f_carrier*pi*t).*RF_signal;
+a=1;
+b=repelem(1/(Fs/2/f_carrier), Fs/2/f_carrier);
+I=filter(b,a,I);
+Q=filter(b,a,Q);
+M = sqrt(I.^2 + Q.^2);
+phi = atan(Q./I);
+subplot(5,2,5)
+plot(t, I)
+title('I')
+subplot(5,2,6)
+plot(t, Q)
+title('Q')
+subplot(5,2,7:8)
+plot(t, M)
+title('M')
+subplot(5,2,9:10)
+plot(t, phi)
+title('phi')
 
 
 
