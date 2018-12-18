d49b7da4ea6ee7fa33266e43e62c3af47e32490c
CephHunter <32884861+CephHunter@users.noreply.github.com>
2018-12-18 04:03:18 +0100

    +

--- main.m
+++ main.m
@@ -1,94 +1,150 @@
 %% AM modulatie
-Fs = 10000;
-t_length = 6; % seconds
-t = 0:1/Fs:t_length-1/Fs;
-N = length(t);
-signal = 0.5*sin(2*pi*t)+0.3*cos(2*pi*5*t);
-f_carrier = 100;
-subplot(3,2,1)
+clear;
+clc;
+Fs = 10000;                 % Sample frequency
+t_length = 3;               % signal length in seconds
+t = 0:1/Fs:t_length-1/Fs;   % time steps
+N = length(t);              % total number of datapoints
+signal = sin(2*pi*t);       % Input signal
+f_carrier = 100;            % carrier frequency
+subplot(5,2,1)
 plot(t,signal)
 title("Signal")
 
-RF_0 = cos(2*pi*f_carrier*t);
-RF_90 = sin(2*pi*f_carrier*t);
-I_in = 0 .* RF_0;
-Q_in = (1+signal) .* RF_90;
-RF_signal = I_in + Q_in;
+% fourrier tranform of the input singnal
+y = fft(signal);            % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,2)
+plot(fshift, abs(yshift))
+axis([0 10 0 t_length*Fs/2])
+title("fft input signal")
 
-% modulated_signal = carrier .* (1 + signal);
-modulated_signal = RF_signal;
-subplot(3,2,2)
+% DSB modulatie
+modulated_signal = (1+signal) .* sin(2*pi*f_carrier*t);
+subplot(5,2,3)
 plot(t, modulated_signal)
 title("Modulated signal")
 
-y = fft(modulated_signal);
-f = (0:N-1)*Fs/N;
-fshift = (-N/2:N/2-1)*Fs/N;
-yshift = fftshift(y);
-subplot(3,2,3)
+% fourrier tranform of the modulated singnal
+y = fft(modulated_signal);  % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,4)
 plot(fshift, abs(yshift))
 axis([75 125 0 t_length*Fs/2])
-title("fft")
+title("fft modulated signal")
 
 % Am demodulation
-I = cos(2*f_carrier*pi*t).*modulated_signal;
-Q = sin(2*f_carrier*pi*t).*modulated_signal;
-received_signal = movmax(sqrt(I.^2 + Q.^2), Fs/2/f_carrier);
+phi = 0;  % phase shift
+IO_0 = cos(2*pi*f_carrier*t + phi);     % internal oscillator
+IO_90 = sin(2*pi*f_carrier*t + phi);
+I = IO_0 .* modulated_signal;
+Q = IO_90 .* modulated_signal;
 
-a=1;
-b=repelem(1/(Fs/2/f_carrier), Fs/2/f_carrier);
-I=filter(b,a,I);
-Q=filter(b,a,Q);
-received_signal=filter(b,a,received_signal);
-
-subplot(3,2,4)
-plot(t,I)
+% filter signal
+% https://stackoverflow.com/questions/1783633/matlab-apply-a-low-pass-or-high-pass-filter-to-an-array
+tau = 0.1;  % low pass filter time constant
+a = (1/Fs)/tau;
+I_filtered = filter(a, [1 a-1], I);
+Q_filtered = filter(a, [1 a-1], Q);
+received_signal = sqrt(I_filtered.^2 + Q_filtered.^2);
+subplot(5,2,5)
+plot(t, I_filtered)
 title("I")
-subplot(3,2,5)
-plot(t,Q)
+subplot(5,2,6)
+plot(t, Q_filtered)
 title("Q")
-subplot(3,2,6)
-plot(t,received_signal)
-title("Recieved signal")
+subplot(5,2,7:8)
+plot(t, received_signal)
+title("Received signal")
+
+% fourrier tranform of the demodulated singnal
+y = fft(received_signal);   % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,9)
+plot(fshift, abs(yshift))
+axis([0 10 0 t_length*Fs/2])
+title("fft received signal")
+subplot(5,2,10)
+plot(fshift, abs(yshift))
+axis([190 210 0 t_length*Fs/20])
+title("fft received signal")
+
 
 %% FM modulatie
 clear;
 clc;
-Fs = 1000;
-t_length = 6; % seconds
-t = 0:1/Fs:t_length-1/Fs;
-N = length(t);
-signal = sin(2*pi*t)+0.5*cos(2*pi*5*t);
-f_carrier = 100;
-subplot(5,2,1:2)
+Fs = 10000;                 % Sample frequency
+t_length = 2;               % signal length in seconds
+t = 0:1/Fs:t_length-1/Fs;   % time steps
+N = length(t);              % total number of datapoints
+signal = sin(2*pi*t);       % Input signal
+f_carrier = 100;            % carrier frequency
+subplot(5,2,1)
 plot(t,signal)
 title('signal')
 
-RF_signal = cos(2*pi*f_carrier*t + 1*signal);
-subplot(5,2,3:4)
+% fourrier tranform of the input singnal
+y = fft(signal);            % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,2)
+plot(fshift, abs(yshift))
+axis([0 10 0 t_length*Fs/2])
+title("fft input signal")
+
+% FM modulation
+RF_signal = cos(2*pi*f_carrier*t + signal);
+subplot(5,2,3)
 plot(t, RF_signal)
 title('RF modulated')
 
-I = cos(2*f_carrier*pi*t).*RF_signal;
-Q = sin(2*f_carrier*pi*t).*RF_signal;
-a=1;
-b=repelem(1/(Fs/2/f_carrier), Fs/2/f_carrier);
-I=filter(b,a,I);
-Q=filter(b,a,Q);
-M = sqrt(I.^2 + Q.^2);
-phi = atan(Q./I);
+% fourrier tranform of the modulated singnal
+y = fft(RF_signal);         % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,4)
+plot(fshift, abs(yshift))
+axis([75 125 0 t_length*Fs/2])
+title("fft modulated signal")
+
+% IQ demodulation
+phi = 0.0;  % phase shift
+IO_0 = cos(2*pi*f_carrier*t + phi);     % internal oscillator
+IO_90 = sin(2*pi*f_carrier*t + phi);
+I = IO_0 .* RF_signal;
+Q = IO_90 .* RF_signal;
+
+% filter signal
+a = (1/Fs)/0.1;
+I_filtered = filter(a, [1 a-1], I);
+Q_filtered = filter(a, [1 a-1], Q);
+M = sqrt(I_filtered.^2 + Q_filtered.^2);
+phi = atan(Q_filtered./I_filtered);
 subplot(5,2,5)
-plot(t, I)
+plot(t, I_filtered)
 title('I')
 subplot(5,2,6)
-plot(t, Q)
+plot(t, Q_filtered)
 title('Q')
 subplot(5,2,7:8)
-plot(t, M)
-title('M')
-subplot(5,2,9:10)
 plot(t, phi)
-title('phi')
+title("Received signal")
+
+% fourrier tranform of the demodulated singnal
+y = fft(phi);               % fft
+fshift = (-N/2:N/2-1)*Fs/N; % frequency axis centered around 0
+yshift = fftshift(y);       % fft centered around 0
+subplot(5,2,9)
+plot(fshift, abs(yshift))
+axis([0 10 0 t_length*Fs/2])
+title("fft received signal")
+subplot(5,2,10)
+plot(fshift, abs(yshift))
+axis([190 210 0 t_length*Fs/20])
+title("fft received signal")
 
 
 
